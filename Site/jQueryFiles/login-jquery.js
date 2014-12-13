c1067869ace53d36502eee8278317828c46a2fd9
Tyler Steiner <steinertyler@gmail.com>
2014-12-13 15:18:48 -0600

    php install

---
+++ Site/jQueryFiles/login-jquery.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+
+<!-- PNG FIX for IE6 -->
+<!-- http://24ways.org/2007/supersleight-transparent-png-in-ie6 -->
+<!--[if lte IE 6]>
+<script type="text/javascript" src="login_panel/js/pngfix/supersleight-min.js"></script>
+<![endif]-->
+
+<script src="login_panel/js/slide.js" type="text/javascript"></script>
+
+<?php echo $script; ?>
