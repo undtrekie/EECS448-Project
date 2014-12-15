d89a2c791edac8d93987c14ef1e0504135cccb04
nRoudebush <nroudebush@gmail.com>
2014-12-15 17:44:24 -0600

    test2

--- Site/login.php
+++ Site/login.php
@@ -2,12 +2,5 @@
 $dir = 'accounts/';
 $accounts1 = scandir($dir);
 if (in_array(['field3' . '-' . 'field4' . '.txt'],$accounts1, true))
-{
-  fwrite('success.txt','field3' . '-' . 'field4' . '.txt')
-}
-else
-{
-  echo "Incorrect Account or Password";
-}
-
+fwrite('success.txt','field3' . '-' . 'field4' . '.txt')
 ?>
