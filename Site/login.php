786add6e526468b56b3c9c9fae0e06756acab840
nRoudebush <nroudebush@gmail.com>
2014-12-15 17:31:11 -0600

    .

--- Site/login.php
+++ Site/login.php
@@ -1,9 +1,9 @@
 <?php
-$dir = '/accounts';
+$dir = 'accounts/';
 $accounts1 = scandir($dir);
-if (in_array(['field1' . '-' . 'field2' . '.txt'],$accounts1, true))
+if (in_array(['field3' . '-' . 'field4' . '.txt'],$accounts1, true))
 {
-  fwrite('success.txt','field1' . '-' . 'field2' . '.txt')
+  fwrite('success.txt','field3' . '-' . 'field4' . '.txt')
 }
 else
 {
