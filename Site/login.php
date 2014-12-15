eb092bfb5508a2c2bed0a8c5ad031f64397173a0
nRoudebush <nroudebush@gmail.com>
2014-12-15 17:54:18 -0600

    test3

--- Site/login.php
+++ Site/login.php
@@ -2,5 +2,12 @@
 $dir = 'accounts/';
 $accounts1 = scandir($dir);
 if (in_array(['field3' . '-' . 'field4' . '.txt'],$accounts1, true))
-fwrite('success.txt','field3' . '-' . 'field4' . '.txt')
+{
+  $my_file = 'success.txt';
+  $handle = fopen($my_file, 'w') or die('Cannot open file: DNE');
+  fwrite($handle,'field3' . '-' . 'field4' . '.txt')
+  fclose($handle);
+} else {
+  echo "Wrong account number or password!";
+}
 ?>
