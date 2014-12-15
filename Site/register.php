857659d3279406bd7b4e562c9d4329a659fb8eef
bkolega <kolanite13@gmail.com>
2014-12-14 21:33:53 -0600

    Registering Process
    Generates account number based on time, then allows user to create
    password and initial balance.  The file generated then is
    accountnumber-password.txt and the contents is the initial balance.
    Needs to formatting changes to match others but other than that, it
    works

---
+++ Site/register.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_POST['field1']) && isset($_POST['field2']) && isset($_POST['field3'])) {
+        $data = $_POST['field3'] . " \n";
+		$filename = $_POST['field1'] . '-' . $_POST['field2'] . ".txt";
+        $ret = file_put_contents($filename, $data, FILE_APPEND | LOCK_EX);
+        if($ret === false) {
+            die('There was an error writing this file');
+        }
+        else {
+            echo "$ret bytes written to file";
+        }
+    }
+    else {
+        die('no post data to process');
+    }
+	
+	
+?>
