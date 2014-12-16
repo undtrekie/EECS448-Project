a83fbc32636cc7e3a8e909f76132a7ac415edc26
Tyler Steiner <steinertyler@gmail.com>
2014-12-15 18:57:16 -0600

    Final Login Commit

--- Site/login.php
+++ Site/login.php
@@ -1,13 +1,18 @@
 <?php
 $dir = 'accounts/';
 $accounts1 = scandir($dir);
-if (in_array(['field3' . '-' . 'field4' . '.txt'],$accounts1, true))
+$file = 'accounts/success.txt';
+$data = $_POST['field4'] . '-' . $_POST['field5'] . '.txt';
+if (in_array($data,$accounts1, false))
 {
-  $my_file = 'accounts/success.txt';
-  $handle = fopen($my_file, 'w') or die('Cannot open file: DNE');
-  file_put_contents($handle,'field3' . '-' . 'field4' . '.txt')
-  fclose($handle);
+
+file_put_contents($file,$data, LOCK_EX);
+header( 'Location: https://undtrekie.no-ip.biz/AccountView.html' ) ;
+
 } else {
-  echo "Wrong account number or password!";
+var_dump($accounts1);
+
+header( 'Location: https://undtrekie.no-ip.biz/' ) ;
+
 }
 ?>
