43f141b27fb0462d15b612d38cc5a1246a3035c7
Tyler Steiner <steinertyler@gmail.com>
2014-12-15 20:14:56 -0600

    Lots of Chages

--- Site/AccountView.php
+++ Site/AccountView.php
@@ -1,12 +1,17 @@
 <?php
-	$arr = json_decode($_POST['jsondata']);
-	$filename = $_POST['field1'];
-	$ret = file_put_contents($filename, $arr, FILE_APPEND | LOCK_EX);
+    $filename = $_POST['field1'];
+	$arr = $_POST['field2'];
+	$arr = str_replace('[','',$arr);
+	$arr = str_replace(']','',$arr);
+	$arr = str_replace('"','',$arr);
+	$arr = str_replace(',',"\n",$arr);
+    $ret = file_put_contents($filename, $arr, LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }
     else {
         echo "$ret bytes written to file";
-        header( 'Location: https://undtrekie.no-ip.biz/' );
+		header('Location:https://undtrekie.no-ip.biz/');
+		//echo "$arr";
     }
 ?>
